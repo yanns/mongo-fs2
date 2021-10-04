256fbd55543d6f94318300d952084711639b4df5
Yann Simon <yann.simon@commercetools.com>
2021-10-04 17:04:22 +0200

    insert many

--- src/main/scala/mongo_fs2/Populate.scala
+++ src/main/scala/mongo_fs2/Populate.scala
@@ -3,10 +3,11 @@ package mongo_fs2
 import cats.effect.{IO, IOApp}
 import org.mongodb.scala.MongoCollection
 import org.mongodb.scala.bson.collection.immutable.Document
-import org.mongodb.scala.result.InsertOneResult
+import org.mongodb.scala.result.{InsertManyResult, InsertOneResult}
 
 object Populate extends IOApp.Simple {
   val toInsert = 5_000
+  val chunkSize = 100
   val outputEvery = 100
 
   override def run: IO[Unit] = {
@@ -22,22 +23,30 @@ object Populate extends IOApp.Simple {
           .mapAsync(1) { i =>
             for {
               _ <- if (i % 100 == 0) IO.print(".") else IO.unit
-              _ <- insert(collection)(i)
+              _ <- insertMany(collection)(i, chunkSize)
             } yield 1L
           }
           .foldMonoid
           .compile
           .lastOrError
         count <- IO.fromFuture(IO(collection.countDocuments().head()))
-        _ <- IO.println(s"\ninserted $count items (should: $toInsert)")
+        _ <- IO.println(
+          s"\ninserted $count items (should: ${toInsert * chunkSize}"
+        )
       } yield ()
     }
   }
 
-  private def insert(
+  private def insertMany(
       collection: MongoCollection[Document]
-  )(i: Int): IO[InsertOneResult] =
+  )(from: Int, nbr: Int): IO[InsertManyResult] =
     IO.fromFuture(
-      IO(collection.insertOne(Document("name" -> s"pif-$i")).head())
+      IO(
+        collection
+          .insertMany(
+            (from.to(from + nbr)).map(i => Document("name" -> s"pif-$i"))
+          )
+          .head()
+      )
     )
 }
